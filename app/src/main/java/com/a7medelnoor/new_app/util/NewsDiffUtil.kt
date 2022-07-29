8837d747230477123a632344762fd9e68629c676
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2022-07-29 16:31:03 +0400

    create diff util class for our recyclerview

---
+++ app/src/main/java/com/a7medelnoor/new_app/util/NewsDiffUtil.kt
@@ -0,0 +1,25 @@
+package com.a7medelnoor.new_app.util
+
+import androidx.recyclerview.widget.DiffUtil
+import com.a7medelnoor.new_app.data.remote.model.Result
+
+class NewsDiffUtil(
+    private val oldList: List<com.a7medelnoor.new_app.data.remote.model.Result>,
+    private val newList: List<Result>
+) : DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+        return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition] === newList[newItemPosition]
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition] == newList[newItemPosition]
+    }
+}
