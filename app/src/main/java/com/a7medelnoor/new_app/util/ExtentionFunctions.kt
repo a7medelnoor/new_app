078082b20ed30942c75104a51ff2877ccb30b016
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2022-07-30 23:57:02 +0400

    extensions functions

---
+++ app/src/main/java/com/a7medelnoor/new_app/util/ExtentionFunctions.kt
@@ -0,0 +1,46 @@
+package com.a7medelnoor.new_app.util
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
+
+fun <T> LiveData<T>.observeOnce(lifeCycleOwner: LifecycleOwner, observer: Observer<T>) {
+    observe(lifeCycleOwner, object : Observer<T> {
+        override fun onChanged(t: T) {
+            removeObserver(this)
+            observer.onChanged(t)
+        }
+    })
+}
+fun <T> LiveData<T>.getOrAwaitValue(
+    time: Long = 2,
+    timeUnit: TimeUnit = TimeUnit.SECONDS,
+    afterObserve: () -> Unit = {}
+): T {
+    var data: T? = null
+    val latch = CountDownLatch(1)
+    val observer = object : Observer<T> {
+        override fun onChanged(t: T?) {
+            data = t
+            latch.countDown()
+            this@getOrAwaitValue.removeObserver(this)
+        }
+    }
+    this.observeForever(observer)
+    try {
+        afterObserve.invoke()
+        if (!latch.await(time, timeUnit)) {
+            throw TimeoutException("LiveData value was never set.")
+        }
+    } finally {
+        this.removeObserver(observer)
+    }
+    @Suppress("UNCHECKED_CAST")
+
+    return data as T
+
+
+}
