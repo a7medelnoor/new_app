09f0a004c55a0eadd85bcc3b2862f14cb0c25fb2
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2022-07-30 22:35:12 +0400

    add database and table name

--- app/src/main/java/com/a7medelnoor/new_app/util/Constants.kt
+++ app/src/main/java/com/a7medelnoor/new_app/util/Constants.kt
@@ -2,4 +2,6 @@ package com.a7medelnoor.new_app.util
 
 object Constants {
     const val BASE_URL = "https://api.nytimes.com/svc/mostpopular/v2/mostviewed/"
+    const val TABLE_NAME = "articles_table"
+    const val DATABASE_NAME = "articles_database"
 }
