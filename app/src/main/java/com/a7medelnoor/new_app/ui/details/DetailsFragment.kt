0fae90a989ecc59a349a95fba68f5fcc1219bd25
Ahmed Elnoor <acc.ahmedelnoor.adam@gmail.com>
2022-07-31 15:48:43 +0400

    handle empty and null list and populate the ui

--- app/src/main/java/com/a7medelnoor/new_app/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/a7medelnoor/new_app/ui/details/DetailsFragment.kt
@@ -6,9 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
-import coil.load
 import com.a7medelnoor.new_app.R
-import com.a7medelnoor.new_app.data.remote.model.NewsResponse
 import com.a7medelnoor.new_app.data.remote.model.Result
 import com.a7medelnoor.new_app.databinding.FragmentDetailsBinding
 import com.squareup.picasso.Picasso
@@ -18,7 +16,9 @@ import dagger.hilt.android.AndroidEntryPoint
 class DetailsFragment : Fragment() {
     private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
-    private val  args: DetailsFragmentArgs by navArgs()
+    private val args: DetailsFragmentArgs by navArgs()
+
+
     private lateinit var articlesData: Result
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,9 +26,9 @@ class DetailsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentDetailsBinding.inflate(
-            inflater,container,false
+            inflater, container, false
         )
-      return binding.root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -41,17 +41,34 @@ class DetailsFragment : Fragment() {
 
     private fun populateDetailsUI() {
         // populate the data to details
-       binding.apply {
-           Picasso.get().load(articlesData.media[0].MediaMetadata[0].url)
-               .error(R.drawable.ic_placeholder)
-               .into(articleDetailsImageView)
-           articlesDetailsTitle.text = articlesData.title
-           articleDetailsByLine.text = articlesData.byline
-           articleDetailsSection.text = articlesData.section
-           val articleDetails = articlesData.published_date
-           articleDetailsDateTextView.text = articleDetails
-       }
+        binding.apply {
+            // check null and empty lists for articles, media and meta data
+            if (articlesData.media != null && !articlesData.media.isEmpty() &&
+                articlesData.media.get(0).MediaMetadata != null &&
+                !articlesData.media.get(0).MediaMetadata.isEmpty()
+            ) {
+                // load the image from url
+
+                Picasso.get().load(articlesData.media[0].MediaMetadata[0].url)
+                    .error(R.drawable.ic_placeholder)
+                    .into(articleDetailsImageView)
+            } else {
+                // error load placeholder image
+
+                Picasso.get().load(R.drawable.ic_placeholder)
+                    .error(R.drawable.ic_placeholder)
+                    .into(articleDetailsImageView)
+            }
+            // bind the details data
+
+            articlesDetailsTitle.text = articlesData.title
+            articleDetailsByLine.text = articlesData.byline
+            articleDetailsSection.text = articlesData.section
+            val articleDetails = articlesData.published_date
+            articleDetailsDateTextView.text = articleDetails
+        }
     }
+
     // clear the binding
     override fun onDestroy() {
         super.onDestroy()
